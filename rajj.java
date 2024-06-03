0395aeea187758bbc27617d9469287a9672e319b
Srirajkumar3 <rajkumardavu1-@gmail.com>
2024-06-03 13:58:37 +0530

    Add rajj.java

---
+++ rajj.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class rajj{
+public static void main(String[] args) {
+	int arr[]= {1,2,3,4,3,5,1,2,4,5,7};
+	Map<Integer, Integer> mp=new  HashMap<Integer, Integer>();
+	for(int i=0;i<arr.length;i++) {
+		int x=mp.getOrDefault(arr[i], 0)+1;
+		
+	}
+    System.out.println(mp);
+}
+}
